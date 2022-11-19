6b6433a705d628b327ddb504f4e5743fb068df45
Cristian Mesa Sanchez <cristian.mesa.sanchez@estudiantat.upc.edu>
2022-11-19 19:28:40 +0100

    Admin users get admin automatically when creating a profile or reloading the app TG-229 #closed

--- src/main/java/com/potus/app/config/InitialDataConfiguration.java
+++ src/main/java/com/potus/app/config/InitialDataConfiguration.java
@@ -2,6 +2,7 @@ package com.potus.app.config;
 
 import com.potus.app.airquality.model.Region;
 import com.potus.app.airquality.service.AirQualityService;
+import com.potus.app.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +22,12 @@ public class InitialDataConfiguration {
     @Autowired
     AirQualityService airQualityService;
 
+    @Autowired
+    UserService userService;
+
     @Bean
     CommandLineRunner runner(){
+        userService.addAdmins();
         List<Region> regions = airQualityService.findAll();
 
         if(regions.size() == 0) {
