a23c218ef5281c346cc5d234f6324ec0e081ab7d
Bagansio <29057684+Bagansio@users.noreply.github.com>
2023-01-08 01:59:53 +0100

    Fix revive bug TG-279 #closed

--- src/main/java/com/potus/app/potus/service/PotusService.java
+++ src/main/java/com/potus/app/potus/service/PotusService.java
@@ -261,10 +261,12 @@ public class PotusService {
         List<PotusModifier> potusModifiers = potusModifierRepository.findByPotus(potus);
         potusModifierRepository.deleteAll(potusModifiers);
 
+        Map<Actions, PotusAction> actions = PotusUtils.generateDefaultActions();
+
         List<Modifier> buffModifiers = modifierRepository.findByModifierType(ModifierType.PERMANENT_BUFF);
 
         potus.setBuffs(generatePotusModifiers(potus,buffModifiers));
-
+        potus.setActions(actions);
         potusModifierRepository.saveAll(potus.getBuffs());
         return potusRepository.save(potus);
     }
